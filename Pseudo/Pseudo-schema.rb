96fa79ca15feba72665a18dc93aaa99b70bcb8d0
bootcoder <bootcoder@gmail.com>
2014-07-03 16:22:31 -0700

    add pseudo file

---
+++ Pseudo/Pseudo-schema.rb
@@ -0,0 +1,65 @@
+users - 
+	username
+	password_hash
+	zip_code
+	birthday
+	email
+	bio
+
+
+
+
+
+
+
+events - 
+	event name
+	event date time
+	event type_id
+	event active boolean
+	event location
+
+
+
+groups
+	has users
+	user has admin boolean
+	
+	group_name
+	
+
+
+
+games
+	name
+	type
+
+
+
+
+
+
+
+
+notes on Travis ci
+
+bake in travis after writing first spec
+
+make repo public and add travsCI under webhooks and services
+
+create travis.yml file
+
+reloase 0 by 530
+repo up and heroku
+
+
+as much project workflow as possible
+
+
+
+
+
+
+
+
+
