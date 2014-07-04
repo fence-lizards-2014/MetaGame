65661278a102dd9b7dbb9162ba6e90e18cd2559f
bootcoder <bootcoder@gmail.com>
2014-07-03 17:49:50 -0700

    Changes .gitignore to allow secret key Heroku needs the secret key bro.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metagame::Application.config.secret_token = 'a0889a025953cda2f3631b8d12d0698727c902a2ce311e2138ebf72cca7328b6d877e5047bbac89f6d3e14b7142b308d87bfa0ae37800fbaa1b21d66d016a91f'
