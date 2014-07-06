9551e7a946fa0e44f039c3de9008b684de53ded5
SamuelDavis <sam9@cfu.net>
2014-07-05 19:22:16 -0700

    fixed type with mass assign

--- spec/features/sessions_features_spec.rb
+++ spec/features/sessions_features_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 feature "get Signin" do 
-	let(:user) { User.new user_name: 'Jake', password_hash: "12345" }
+	let(:user) { User.new username: 'Jake', password_hash: "12345" }
 	let(:input_password) { '12345' }
 		scenario "User is properly signed in when proper info is entered" do
 
 			user.password = "12345"
 			user.save
 			visit root_path
-			fill_in "user_user_name", with: user.user_name
+			fill_in "user_username", with: user.username
 			fill_in "user_password_hash", with: input_password
 			click_button "Submit"
 			expect(page).to have_content "Logout"
