1c519a742cb497f117ef7219059fd6d806eee12c
SamuelDavis <sam9@cfu.net>
2014-07-05 21:54:43 -0700

    get games by console added for giant bomb api, tests up and running as well

--- app/models/giant_bomb_adapter.rb
+++ app/models/giant_bomb_adapter.rb
@@ -7,24 +7,34 @@ class GiantBombAdapter
 	base_uri 'http://www.giantbomb.com/api'
 
 	INVALID_TITLE = 'No such game exists! Please try again!'
+	INVALID_ID = 'No such console exists!'
 
-	def initialize title
+	def initialize title, game_id
 		@key = '5a4c47474d09321305c85ae777a19fdf647d8257'
 		@title = URI.escape title
+		@game_id = game_id
 	end
 
 	def search
-		self.class.get "/#{get_type}#{get_ids}&#{format}&#{query}&#{resource}" || OpenStruct.new(title: INVALID_TITLE)
+		self.class.get "/#{get_type}?#{get_ids}&#{format}&#{query}&#{resource}" || OpenStruct.new(title: INVALID_TITLE)
+	end
+
+	def get_games_by_console
+		self.class.get "/#{game_type}/?#{format}&#{get_ids}&#{filter}&#{field_list}"
 	end
 
 	private
+
+	def game_type
+		"games"
+	end
 
 	def get_type
 		'search'
 	end
 
 	def get_ids
-		"?api_key=#{@key}"
+		"api_key=#{@key}"
 	end
 
 	def format
@@ -39,9 +49,32 @@ class GiantBombAdapter
 		'resources=game'
 	end
 
+	def filter
+		"filter=platforms:#{@game_id}"
+	end
+
+	def field_list
+		"field_list=genres,name"
+	end
+
 end
 
-# http://www.giantbomb.com/api/search?api_key=5a4c47474d09321305c85ae777a19fdf647d8257&format=json&query=banjo-kazooie&resources=game
+# http://www.giantbomb.com/api/games/?format=json&api_key=5a4c47474d09321305c85ae777a19fdf647d8257&filter=platforms:43&field_list=genres,name
 
-# GiantBombAdapter.new('some title').search
+# nes - 21
+# snes - 9
+# n64 - 43
+# gamecube - 23
+# wii - 36
+# wii u - 139
 
+# ps1 - 22
+# ps2 - 19
+# ps3 - 88 
+# ps4 - 146
+
+# xbox - 32
+# xbox 360 - 20
+# xbox one - 145
+
+# pc - 94
--- spec/models/giant_bomb_adapter_spec.rb
+++ spec/models/giant_bomb_adapter_spec.rb
@@ -5,9 +5,11 @@ describe GiantBombAdapter do
 
 	GAME_TITLE = 'Banjo-Kazooie'
 	INVALID_GAME_TITLE = ''
+  PLATFORM_ID = '43' # Nintendo 64
+  INVALID_PLATFORM_ID = ''
 
 	context "when searching for a game when valid title" do
-    let(:game) { GiantBombAdapter.new(GAME_TITLE).search }
+    let(:game) { GiantBombAdapter.new(GAME_TITLE, PLATFORM_ID).search }
 
     before(:each) do
       allow_any_instance_of(GiantBombAdapter).to receive(:search).and_return(OpenStruct.new title: GAME_TITLE )
@@ -20,7 +22,7 @@ describe GiantBombAdapter do
   end
 
   context "when searching for a game when invalid title" do
-    let(:game) { GiantBombAdapter.new(INVALID_GAME_TITLE).search }
+    let(:game) { GiantBombAdapter.new(INVALID_GAME_TITLE, INVALID_PLATFORM_ID).search }
 
     before(:each) do
       allow_any_instance_of(GiantBombAdapter).to receive(:search).and_return(OpenStruct.new title: GiantBombAdapter::INVALID_TITLE )
@@ -28,6 +30,32 @@ describe GiantBombAdapter do
 
     it 'should return the null object pattern user' do
       expect(game.title).to eq GiantBombAdapter::INVALID_TITLE
+    end
+
+  end
+
+  context "when searching for a console's games when valid console_id" do
+    let(:game) { GiantBombAdapter.new(GAME_TITLE, PLATFORM_ID).get_games_by_console }
+
+    before(:each) do
+      allow_any_instance_of(GiantBombAdapter).to receive(:get_games_by_console).and_return(OpenStruct.new platform_id: PLATFORM_ID )
+    end
+
+    it 'should return the correct game title' do
+      expect(game.platform_id).to eq PLATFORM_ID
+    end
+
+  end
+
+  context "when searching for a console's games when invalid platform id" do
+    let(:game) { GiantBombAdapter.new(INVALID_GAME_TITLE, INVALID_PLATFORM_ID).get_games_by_console }
+
+    before(:each) do
+      allow_any_instance_of(GiantBombAdapter).to receive(:get_games_by_console).and_return(OpenStruct.new platform_id: GiantBombAdapter::INVALID_ID )
+    end
+
+    it 'should return the null object pattern user' do
+      expect(game.platform_id).to eq GiantBombAdapter::INVALID_ID
     end
 
   end
