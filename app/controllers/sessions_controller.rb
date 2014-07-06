e5e8f856e7594fa023edcd4c39e325ebb4fe8dcc
bootcoder <bootcoder@gmail.com>
2014-07-05 18:59:47 -0700

    YAY known sessions cntr change refactored for clarity

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,11 @@ class SessionsController < ApplicationController
 	def auth_callback
 		user = User.find session[:id]
 	  auth = request.env['omniauth.auth']
-	  session[:current_user] = { :nickname => auth.info['nickname'],
-	                                        :image => auth.info['image'],
-	                                      :uid => auth.uid }
+	  session[:current_user] = {  :nickname => auth.info['nickname'],
+																:image => auth.info['image'],
+																:uid => auth.uid }
 	  redirect_to user_path(user)
 	end
-
 
 	def logout
 		session[:current_user] = nil
@@ -24,16 +23,12 @@ class SessionsController < ApplicationController
 	end
 
 	def sign_in_attempt
-		p "*" * 250
-		p params
 		@user = User.find_by_username(params[:user][:username])
 		if @user.password == params[:user][:password_hash]
 			session[:id] = @user.id
-			redirect_to user_path(@user)
+			redirect_to user_path @user
 		else
 			redirect_to sign_in_path, flash: { notice: "Your password was incorrect!" }
 		end
 	end
 end
-
-
