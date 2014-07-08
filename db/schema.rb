840c6e2c250c0803506472433861f59825336e62
Jake Persing <misterdamon@gmail.com>
2014-07-08 11:22:42 -0700

    adding posts to master branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140705204822) do
+ActiveRecord::Schema.define(:version => 20140708034047) do
 
   create_table "events", :force => true do |t|
     t.string   "event_name"
@@ -66,6 +66,14 @@ ActiveRecord::Schema.define(:version => 20140705204822) do
     t.integer  "event_id"
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
+  end
+
+  create_table "posts", :force => true do |t|
+    t.text     "post_content"
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
   create_table "user_admins", :id => false, :force => true do |t|
