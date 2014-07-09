30edf641c328d106a6ea8ed99be37c40df5a1bc0
bootcoder <bootcoder@gmail.com>
2014-07-09 09:30:32 -0700

    disabled firechat but left code base for just in case

--- app/models/firebase_token.rb
+++ app/models/firebase_token.rb
@@ -14,20 +14,22 @@ end
 
 
 
+#PSEUDO SAMPLE
+#
+#
+# class Zipcode
+# 	include HTTParty
+# 	attr_accessor :key, :zipcode
 
-class Zipcode
-	include HTTParty
-	attr_accessor :key, :zipcode
+# 	base_uri 'https://zipcodedistanceapi.redline13.com/rest/'
 
-	base_uri 'https://zipcodedistanceapi.redline13.com/rest/'
+# 	def initialize zipcode
+# 		@zipcode = zipcode
+# 		@key = "xnbUucWA9nby0aIQYHDw7PESptfWkqzSOKMjXJU676E7gnXe5BLcqp4QTkMdLm5z"
+# 	end
 
-	def initialize zipcode
-		@zipcode = zipcode
-		@key = "xnbUucWA9nby0aIQYHDw7PESptfWkqzSOKMjXJU676E7gnXe5BLcqp4QTkMdLm5z"
-	end
-
-	def by_radius distance
-		self.class.get "/#{@key}/radius.json/#{@zipcode}/#{distance}/miles"
-	end
-end
+# 	def by_radius distance
+# 		self.class.get "/#{@key}/radius.json/#{@zipcode}/#{distance}/miles"
+# 	end
+# end
 
