d6f81b3f6ec60d301aca4235130fe630b2466bac
SamuelDavis <sam9@cfu.net>
2014-07-09 11:50:08 -0700

    refactored the post controller tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,24 +1,10 @@
 require 'spec_helper'
 
 describe PostsController do
-
-	let!(:user) { FactoryGirl.create :user }
-	let!(:group) { FactoryGirl.create :group }
-	let!(:post) { FactoryGirl.create :post }
-
 	context 'GET #index' do
 		it 'should return all of the posts' do
 			get :index
 			expect(assigns(:posts)).to eq Post.all
 		end
 	end
-
-	context 'POST #create' do
-		it 'should increase the database count by 1 if valid params' do
-			expect {
-				post :create, post: { page_content: "hello" }
-			}.to change { Post.count }.by(1)
-		end
-	end
-
 end
