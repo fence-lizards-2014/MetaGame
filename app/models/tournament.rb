429b2e377f914400d319e1f09fc2c1b3bc83a422
SamuelDavis <sam9@cfu.net>
2014-07-09 11:05:17 -0700

    added validations to tournaments

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -4,4 +4,8 @@ class Tournament < ActiveRecord::Base
 	attr_accessible :tourney_name, :tourney_rules, :tourney_max_players, :winner
 	
 	has_many :users	
+
+	validates :tourney_name, presence: true
+	validates :tourney_rules, presence: true
+	validates :tourney_max_players, presence: true
 end
