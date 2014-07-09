ce2ab249c54ec3f2ac596e5d512b4999497e898f
SamuelDavis <sam9@cfu.net>
2014-07-09 11:03:26 -0700

    tourney create test green

--- spec/controllers/tournaments_controller_spec.rb
+++ spec/controllers/tournaments_controller_spec.rb
@@ -10,5 +10,13 @@ describe TournamentsController do
 			expect(response).to render_template :new
 		end
 	end
+
+	context 'POST #create' do
+		it 'should increase the database count by 1 if valid params' do
+			expect {
+				post :create, event_id: event, tournament: { tourney_name: "Tourney", tourney_rules: "Win", tourney_max_players: 10 }
+			}.to change { Tournament.count }.by(1)
+		end
+	end
 	
 end
