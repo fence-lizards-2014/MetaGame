b4a55c71d90280a13889d1526f1cc369b09e9944
bootcoder <bootcoder@gmail.com>
2014-07-11 10:28:33 -0700

    fixed tabbing issues in game.rb file

--- app/models/game.rb
+++ app/models/game.rb
@@ -26,10 +26,10 @@ class Game < ActiveRecord::Base
     games = response[:games]["response"]["games"]
     games.each do |game|
       user.games << Game.create(game_name: game["name"],
-                  game_steam_appid: game["appid"], 
-                  game_img_url: "http://media.steampowered.com/steamcommunity/public/images/apps/#{game["appid"]}/#{game["img_logo_url"]}.jpg", 
-                  game_icon_url: "http://media.steampowered.com/steamcommunity/public/images/apps/#{game["appid"]}/#{game["img_icon_url"]}.jpg", 
-                  game_playtime_forever: game["playtime_forever"])
+                                game_steam_appid: game["appid"], 
+                                game_img_url: "http://media.steampowered.com/steamcommunity/public/images/apps/#{game["appid"]}/#{game["img_logo_url"]}.jpg", 
+                                game_icon_url: "http://media.steampowered.com/steamcommunity/public/images/apps/#{game["appid"]}/#{game["img_icon_url"]}.jpg", 
+                                game_playtime_forever: game["playtime_forever"])
     end
   end
 
@@ -41,9 +41,9 @@ class Game < ActiveRecord::Base
       @returned_game = GiantBombAdapter.new(title).search.parsed_response["results"][0]#["description"]    
       if @returned_game
         game = Game.create(game_name: @returned_game["name"],
-                    game_description: @returned_game["description"],
-                    game_img_url: @returned_game['image']["screen_url"],
-                    game_icon_url: @returned_game['image']['icon_url'])
+                           game_description: @returned_game["description"],
+                           game_img_url: @returned_game['image']["screen_url"],
+                           game_icon_url: @returned_game['image']['icon_url'])
         games = []
         games << game if game
       else
